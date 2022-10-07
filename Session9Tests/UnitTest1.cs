a63080581d0b806991bf6b285db6e6962b89d50e
rishi221b <rishida619@gmail.com>
2022-10-07 18:29:40 +0530

    actualResultChanges

--- Session9Tests/UnitTest1.cs
+++ Session9Tests/UnitTest1.cs
@@ -38,7 +38,7 @@ namespace Session9Tests
         public void Test3()
         {
             var result = 2 + 3;
-            Assert.Equal(14, result);
+            Assert.Equal(5, result);
         }
     }
 }
